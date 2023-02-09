c55b42b033ebb0980b4c3d16f2ad19abe66ac8ca
katejoh <kate.oh13@gmail.com>
2023-02-09 22:51:08 +1100

    refactor: specify category in Contentful query

--- api.py
+++ api.py
@@ -12,12 +12,12 @@ client = contentful.Client(SPACE_ID, ACCESS_TOKEN)
 
 
 def getRandomMarketingPost(category):
-    marketingArchives = client.entries(
-        {'content_type': 'marketing_archives', 'limit': 1000})
+    categoryPosts = client.entries(
+        {'content_type': 'marketing_archives', 'limit': 1000, 'select': 'fields', 'fields.category[in]': category})
 
     posts = []
-    for post in marketingArchives:
-        if category in post.fields().get('category') and post.fields().get('img') != None:
+    for post in categoryPosts:
+        if post.fields().get('img') != None:
             posts.append(post)
 
     post = random.choice(posts)
