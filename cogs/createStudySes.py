af031c000ac274fe93d71718d356a7db1b4caa57
Ace Mikunda <z5416988@ad.unsw.edu.au>
2023-03-21 00:12:55 +1100

    refactor: change create-study-session code to better match previous command structure

--- cogs/createStudySes.py
+++ cogs/createStudySes.py
@@ -7,12 +7,14 @@ class createStudySes(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
-    @app_commands.command(name="create-study-session", description=)
+    @app_commands.command(name="create-study-session", description="Create a study session with members")
     async def create_study_session(self, ctx: commands.Context, date: str, start_time: str, end_time: str):
         username = ctx.author.name
         message_content = f"Study Session!\n\n{username} is having a study session for {date} from {start_time} to {end_time} in the WIT Discord Server! Click the buttons below to RSVP.\n\nAttendees (1):\n{username}"
         embed = discord.Embed(description=message_content, color=discord.Color.blue())
-        await ctx.send(embed=embed)        
+        view = MyView()
+        await ctx.send(embed=embed, view=view)
+
 
 class MyView(discord.ui.View):
     def __init__(self):
@@ -32,5 +34,5 @@ class Buttons(commands.Cog):
         await ctx.send("Are you going or not?", view=view)
 
 
-async def setup(bot):
-    bot.add_cog(Buttons(bot))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(createStudySes(bot))
