ad0ae01a5acb6a177723840f8d2ea514568e3947
Ace Mikunda <z5416988@ad.unsw.edu.au>
2023-03-20 23:59:31 +1100

    feat: create slash command 'create-study-session'

--- cogs/createStudySes.py
+++ cogs/createStudySes.py
@@ -1,7 +1,18 @@
 import discord
 from discord.ext import commands
 from discord.ui import Button
+from discord import app_commands
 
+class createStudySes(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+    @app_commands.command(name="create-study-session", description=)
+    async def create_study_session(self, ctx: commands.Context, date: str, start_time: str, end_time: str):
+        username = ctx.author.name
+        message_content = f"Study Session!\n\n{username} is having a study session for {date} from {start_time} to {end_time} in the WIT Discord Server! Click the buttons below to RSVP.\n\nAttendees (1):\n{username}"
+        embed = discord.Embed(description=message_content, color=discord.Color.blue())
+        await ctx.send(embed=embed)        
 
 class MyView(discord.ui.View):
     def __init__(self):
