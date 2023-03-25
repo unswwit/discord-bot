af1212eda8e5e56527268805548958f3bf009267
Ace Mikunda <z5416988@ad.unsw.edu.au>
2023-03-25 19:14:24 +1100

    feat: add comments

--- cogs/createStudySes.py
+++ cogs/createStudySes.py
@@ -2,14 +2,15 @@ import discord
 from discord.ext import commands
 from discord.ui import Button
 from discord import app_commands
+from discord.ext.commands import Context
 
 class createStudySes(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
     @app_commands.command(name="create-study-session", description="Create a study session with members")
-    async def create_study_session(self, int: discord.Interaction, ctx: commands.Context, date: str, start_time: str, end_time: str):
-        username = ctx.user.name
+    async def create_study_session(self, int: discord.Interaction, date: str, start_time: str, end_time: str):
+        username = int.user.name
         message_content = f"Study Session!\n\n{username} is having a study session for {date} from {start_time} to {end_time} in the WIT Discord Server! Click the buttons below to RSVP.\n\nAttendees (1):\n{username}"
         embed = discord.Embed(description=message_content, color=discord.Color.blue())
         view = MyView()
@@ -26,3 +27,9 @@ class MyView(discord.ui.View):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(createStudySes(bot))
+
+# bold text, 
+# add help text/description of what to enter (date...)
+# Enter the date of your study session! (DD/MM/YYY)
+# Enter the starting time of your study session! (Xpm)
+# Enter the ending time of your study session! (Xpm)
