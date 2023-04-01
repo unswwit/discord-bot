bb721e8ca32e4744d22e0ccca4574ebc79c361d7
gracexkan <80564637+gracexkan@users.noreply.github.com>
2023-04-02 00:52:46 +1100

    chore(utils): add linting to GitHub workflow [WIT-96] (#9)
    * added linting
    
    * linted files
    
    * added black to requirements.txt

--- api.py
+++ api.py
@@ -5,19 +5,25 @@ import contentful
 from dotenv import load_dotenv
 
 load_dotenv()
-SPACE_ID = os.getenv('CONTENTFUL_API_SPACE')
-ACCESS_TOKEN = os.getenv('CONTENTFUL_API_TOKEN')
+SPACE_ID = os.getenv("CONTENTFUL_API_SPACE")
+ACCESS_TOKEN = os.getenv("CONTENTFUL_API_TOKEN")
 
 client = contentful.Client(SPACE_ID, ACCESS_TOKEN)
 
 
 def getRandomMarketingPost(category):
     categoryPosts = client.entries(
-        {'content_type': 'marketing_archives', 'limit': 1000, 'select': 'fields', 'fields.category[in]': category})
+        {
+            "content_type": "marketing_archives",
+            "limit": 1000,
+            "select": "fields",
+            "fields.category[in]": category,
+        }
+    )
 
     posts = []
     for post in categoryPosts:
-        if post.fields().get('img') != None:
+        if post.fields().get("img") != None:
             posts.append(post)
 
     return random.choice(posts)
@@ -25,16 +31,17 @@ def getRandomMarketingPost(category):
 
 def getNextUpcomingEvent():
     upcomingEvents = client.entries(
-        {'content_type': 'upcomingEvents', 'limit': 1, 'order': 'fields.index'})
+        {"content_type": "upcomingEvents", "limit": 1, "order": "fields.index"}
+    )
     return upcomingEvents[0]
 
 
 def getUpcomingEvents():
-    return client.entries(
-        {'content_type': 'upcomingEvents', 'order': 'fields.index'})
+    return client.entries({"content_type": "upcomingEvents", "order": "fields.index"})
 
 
 def getMostRecentEvent():
     pastEvents = client.entries(
-        {'content_type': 'pastEvents', 'limit': 1, 'order': '-fields.index'})
+        {"content_type": "pastEvents", "limit": 1, "order": "-fields.index"}
+    )
     return pastEvents[0]
