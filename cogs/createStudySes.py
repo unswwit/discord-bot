6f7a1312876182b062bc86f3e85462d080de3648
aolinxuu <aolinzamxu@gmail.com>
2023-04-02 14:59:57 +1000

    feat interaction with button

--- cogs/createStudySes.py
+++ cogs/createStudySes.py
@@ -1,3 +1,4 @@
+# from discord.ui import Button as UIButton
 # import discord
 # from discord.ext import commands
 # from discord.ui import Button
@@ -49,6 +50,8 @@
 # Enter the date of your study session! (DD/MM/YYY)
 # Enter the starting time of your study session! (Xpm)
 # Enter the ending time of your study session! (Xpm)
+
+from discord.ui import Button as UIButton
 import discord
 from discord.ext import commands
 from discord.ui import Button
@@ -64,30 +67,57 @@ class createStudySes(commands.Cog):
     @app_commands.command(name="create-study-session", description="Create a study session with members")
     async def create_study_session(self, inter: discord.Interaction, date: str, start_time: str, end_time: str):
         username = inter.user.name
-        message_content = f"Study Session!\n\n{username} is having a study session for {date} from {start_time} to {end_time} in the WIT Discord Server! Click the buttons below to RSVP.\n\nAttendees ({len(self.attendees)+1}):\n{username}"
+        message_content = f"Study Session!\n\n{username} is having a study session for {date} from {start_time} to {end_time} in the WIT Discord Server! Click the buttons below to RSVP.\n\nAttendees ({len(self.attendees) + 1}):\n{username}\n{self.get_attendees_list()}"
         embed = discord.Embed(description=message_content,
                               color=discord.Color.blue())
-        view = MyView(self.add_attendee)
+        view = MyView(self.add_attendee, username)
         await inter.response.send_message(embed=embed, view=view)
 
+    # def remove_attendee(self, user: discord.User):
+    #     self.attendees.append(user)
+    #     print(f"Added attendee: {', '.join(self.attendees)}")
+
     def add_attendee(self, user: discord.User):
-        self.attendees.append(user.name)
+        self.attendees.append(user.display_name)
+        # gets rid of duplicates
+        # self.attendees = list(set(self.attendees))
+
+    def get_attendees_list(self):
+        attendees_list = "\n".join(
+            f"- {attendee.name}" for attendee in self.attendees)
+        return attendees_list
 
 
 class MyView(discord.ui.View):
-    def __init__(self, callback):
+    def __init__(self, callback, username):
         super().__init__()
         self.callback = callback
 
-    @discord.ui.button(label="Going", style=discord.ButtonStyle.green, custom_id="going")
-    async def going_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
-        self.callback(interaction.user)
+        self.add_item(Button(style=discord.ButtonStyle.green,
+                      label="Going", custom_id="going"))
+        self.add_item(Button(style=discord.ButtonStyle.red,
+                      label="Not Going", custom_id="not_going"))
 
-    @discord.ui.button(label="Not Going", style=discord.ButtonStyle.red, custom_id="not_going")
-    async def not_going_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
-        self.callback(interaction.user)
+    async def on_button_click(self, button: discord.ui.Button, inter: discord.Interaction):
+        await self.callback(inter, button)
+
+
+# class MyView(discord.ui.View):
+#     def __init__(self, callback, username):
+#         super().__init__()
+#         self.callback = callback
+#         self.username = username
+
+#         self.add_item(Button(style=discord.ButtonStyle.green,
+#                       label="Going", custom_id="going"))
+#         self.add_item(Button(style=discord.ButtonStyle.red,
+#                       label="Not Going", custom_id="not_going"))
+
+#     async def on_button_click(self, button: discord.ui.Button, inter: discord.Interaction):
+#         if button.custom_id == "going":
+#             await self.callback(inter.user, self.username)
+#         self.clear_items()
+#         self.add_item(Label("Thanks for RSVP'ing!"))
 
 
 async def setup(bot: commands.Bot):
