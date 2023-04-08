15e9baa58922d2623eb3afc64fc819c7f7bba1cb
joycceh <122138734+joycceh@users.noreply.github.com>
2023-04-09 05:30:52 +1000

    Update cogs/randomTopicCog.py
    Co-authored-by: katejoh <101443389+katejoh@users.noreply.github.com>

--- cogs/randomTopicCog.py
+++ cogs/randomTopicCog.py
@@ -8,7 +8,7 @@ class randomTopicCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # user types/random_topic and the command will be executed
+    # user types '/random_topic' and the command will be executed
     @app_commands.command(name="random-topic")
     async def random_topic_command(self, int: discord.Interaction):
         # generates a random topic from random_topic module and saves it to a variable called topic
