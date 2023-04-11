f3abe65b302f6c887b36eeb2143804c821d596e3
aolinxuu <aolinzamxu@gmail.com>
2023-04-11 22:25:05 +1000

    fix: lint errors

--- cogs/createStudySession.py
+++ cogs/createStudySession.py
@@ -9,13 +9,18 @@ class createStudySessionCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="create-study-session", description="Schedule a study session and invite WIT Discord members!")
+    @app_commands.command(
+        name="create-study-session",
+        description="Schedule a study session and invite WIT Discord members!",
+    )
     @app_commands.describe(
         date="The date of your study session, formatted as YYYY-MM-DD. (e.g. 2023-04-10)",
         start_time="The starting time of your study session, formatted as HH:MM. (e.g. 13:30)",
         end_time="The ending time of your study session, formatted as HH:MM. (e.g. 14:30)",
     )
-    async def create_study_session(self, inter: discord.Interaction, date: str, start_time: str, end_time: str):
+    async def create_study_session(
+        self, inter: discord.Interaction, date: str, start_time: str, end_time: str
+    ):
         # Discord timestamp formatting
         startUnix = time.mktime(time.strptime(f"{date} {start_time}", "%Y-%m-%d %H:%M"))
         startString = f"<t:{math.floor(startUnix)}:F>"
@@ -26,11 +31,15 @@ class createStudySessionCog(commands.Cog):
         messageContent = f"""
         **🧡 Study Session! 🧡**\n\n<@{id}> is having a study session on {startString} until {endString} in the WIT Discord Server!\n\nClick the buttons below to RSVP.\n\n
         """
-        embed = discord.Embed(title=f"Attendees (1):",
-                              description=f"\n\n- <@{id}>",
-                              color=discord.Color.orange())
+        embed = discord.Embed(
+            title=f"Attendees (1):",
+            description=f"\n\n- <@{id}>",
+            color=discord.Color.orange(),
+        )
         view = MyView(id)
-        await inter.response.send_message(content=messageContent, embed=embed, view=view)
+        await inter.response.send_message(
+            content=messageContent, embed=embed, view=view
+        )
 
 
 class MyView(discord.ui.View):
@@ -39,22 +48,23 @@ class MyView(discord.ui.View):
         self.attendees = {creatorId}  # initialize set of attendees
 
     @discord.ui.button(style=discord.ButtonStyle.green, label="Going")
-    async def going(self,  inter: discord.Interaction, button: discord.ui.Button):
+    async def going(self, inter: discord.Interaction, button: discord.ui.Button):
         self.attendees.add(inter.user.id)
         await self.updateMessage(inter)
 
     @discord.ui.button(style=discord.ButtonStyle.red, label="Not Going")
-    async def notGoing(self,  inter: discord.Interaction, button: discord.ui.Button):
+    async def notGoing(self, inter: discord.Interaction, button: discord.ui.Button):
         self.attendees.discard(inter.user.id)
         await self.updateMessage(inter)
 
     async def updateMessage(self, inter: discord.Interaction):
-        attendeesList = "\n".join(
-            f"- <@{attendee}>" for attendee in self.attendees)
+        attendeesList = "\n".join(f"- <@{attendee}>" for attendee in self.attendees)
 
-        embed = discord.Embed(title=f"Attendees ({len(self.attendees)}):",
-                              color=discord.Color.orange(),
-                              description=f"\n\n{attendeesList}")
+        embed = discord.Embed(
+            title=f"Attendees ({len(self.attendees)}):",
+            color=discord.Color.orange(),
+            description=f"\n\n{attendeesList}",
+        )
         await inter.response.edit_message(embed=embed)
 
 async def setup(bot: commands.Bot):
