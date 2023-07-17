db7d23f4065d0b0f18211559bb5478ee398896c9
aolinxuu <123291831+aolinxuu@users.noreply.github.com>
2023-07-17 12:53:19 +1000

    Feat/wit 116 hangman command button (#36)
    * chore(deps): update dependency discord.py to v2.3.1 (#31)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update dependency charset-normalizer to v3 (#32)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update dependency black to v23.7.0 (#33)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * feat:hangman buttons
    
    * fix: black test
    
    * refacor: alphabet to use lib
    
    * fix: lint
    
    * chore: change lowercase letters to uppercase
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: katejoh <kate.oh13@gmail.com>

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -1,19 +1,19 @@
 import discord
+import string
 from discord.ext import commands
 from discord import app_commands
 from wonderwords import RandomWord
+
 
 class hangmanCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(
-        name="play-hangman", description="Play a game of Hangman!"
-    )
+    @app_commands.command(name="play-hangman", description="Play a game of Hangman!")
     @app_commands.describe(
         category="Choose a category for the random word.",
-        word_length="The length of the random word as an integer or 'Random' for any word length. (e.g. 7)"
-        )
+        word_length="The length of the random word as an integer or 'Random' for any word length. (e.g. 7)",
+    )
     @app_commands.choices(
         category=[
             app_commands.Choice(name="Noun", value=1),
@@ -23,7 +23,10 @@ class hangmanCog(commands.Cog):
         ],
     )
     async def playHangman(
-        self, inter: discord.Interaction, category: app_commands.Choice[int], word_length: str
+        self,
+        inter: discord.Interaction,
+        category: app_commands.Choice[int],
+        word_length: str,
     ):
         await inter.response.defer()
         r = RandomWord()
@@ -43,13 +46,21 @@ class hangmanCog(commands.Cog):
             try:
                 selectedLength = int(word_length)
             except ValueError:
-                await inter.followup.send("I can't generate a word unless word_length is an integer or 'Random', please try again.")
+                await inter.followup.send(
+                    "I can't generate a word unless word_length is an integer or 'Random', please try again."
+                )
                 return
 
         try:
-            randomWord = r.word(include_parts_of_speech=selectedCategory, word_min_length=selectedLength, word_max_length=selectedLength)
+            randomWord = r.word(
+                include_parts_of_speech=selectedCategory,
+                word_min_length=selectedLength,
+                word_max_length=selectedLength,
+            )
         except Exception as e:
-            await inter.followup.send("I can't generate a word with that word length, please try again.")
+            await inter.followup.send(
+                "I can't generate a word with that word length, please try again."
+            )
             return
 
         embed = discord.Embed(
@@ -57,10 +68,87 @@ class hangmanCog(commands.Cog):
             description=f"Selected category: {category.name}\nRandom word: {randomWord}",
             color=discord.Color.orange(),
         )
-
+        view = MyView(id)
         await inter.followup.send(
             embed=embed,
+            view=view,
         )
+
+
+class MyView(discord.ui.View):
+    def __init__(self, creatorId):
+        super().__init__(timeout=None)
+        self.creatorId = creatorId
+        self.pages = []  # list to store button pages
+        self.page_size = 13  # Number of buttons per page
+
+        self.create_buttons()
+        self.split_buttons_into_pages()
+
+        self.current_page = 0
+        self.add_buttons_to_current_page()
+        self.add_page_switching_buttons()
+
+    def create_buttons(self):
+        self.buttons = []
+        for letter in list(string.ascii_uppercase):
+            button_callback = self.create_button_callback(letter)
+            button = discord.ui.Button(
+                style=discord.ButtonStyle.green, label=letter, custom_id=letter
+            )
+            button.callback = button_callback
+            self.buttons.append(button)
+
+    def split_buttons_into_pages(self):
+        self.pages = [
+            self.buttons[i : i + self.page_size]
+            for i in range(0, len(self.buttons), self.page_size)
+        ]
+
+    def create_button_callback(self, button_id):
+        async def button_callback(interaction):
+            await self.updateMessage(interaction)
+
+        return button_callback
+
+    # async def updateMessage(self, interaction):
+
+    def add_page_switching_buttons(self):
+        if self.current_page > 0:
+            previous_button = discord.ui.Button(
+                style=discord.ButtonStyle.blurple,
+                label="Previous",
+                custom_id="previous",
+            )
+            previous_button.callback = self.previous_page
+            self.add_item(previous_button)
+
+        if self.current_page < len(self.pages) - 1:
+            next_button = discord.ui.Button(
+                style=discord.ButtonStyle.blurple, label="Next", custom_id="next"
+            )
+            next_button.callback = self.next_page
+            self.add_item(next_button)
+
+    async def previous_page(self, interaction):
+        if self.current_page > 0:
+            self.current_page -= 1
+            await self.update_view(interaction)
+
+    async def next_page(self, interaction):
+        if self.current_page < len(self.pages) - 1:
+            self.current_page += 1
+            await self.update_view(interaction)
+
+    async def update_view(self, interaction):
+        self.clear_items()
+        self.add_buttons_to_current_page()
+        self.add_page_switching_buttons()
+        await interaction.message.edit(view=self)
+
+    def add_buttons_to_current_page(self):
+        for button in self.pages[self.current_page]:
+            self.add_item(button)
 
 
 async def setup(bot: commands.Bot):
