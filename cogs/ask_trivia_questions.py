2964a6ba9698b9af9eab087248b22cc6c12b33e0
Joyce <joyce.cy.he@gmail.com>
2023-09-05 22:30:05 +1000

    Feat: Add category parameter

--- cogs/ask_trivia_questions.py
+++ cogs/ask_trivia_questions.py
@@ -3,6 +3,33 @@ from discord.ext import commands
 from discord import app_commands
 from pyopentdb import OpenTDBClient, Category, QuestionType, Difficulty
 
+# Define category values
+category_mapping = {
+    1: Category.GENERAL_KNOWLEDGE,
+    2: Category.ENTERTAINMENT_BOOKS,
+    3: Category.ENTERTAINMENT_FILM,
+    4: Category.ENTERTAINMENT_MUSIC,
+    5: Category.ENTERTAINMENT_MUSICALS_THEATRES,
+    6: Category.ENTERTAINMENT_TELEVISION,
+    7: Category.ENTERTAINMENT_VIDEO_GAMES,
+    8: Category.ENTERTAINMENT_BOARD_GAMES,
+    9: Category.SCIENCE_NATURE,
+    10: Category.SCIENCE_COMPUTERS,
+    11: Category.MYTHOLOGY,
+    12: Category.SPORTS,
+    13: Category.GEOGRAPHY,
+    14: Category.HISTORY,
+    15: Category.POLITICS,
+    16: Category.ART,
+    17: Category.CELEBRITIES,
+    18: Category.ANIMALS,
+    19: Category.VEHICLES,
+    20: Category.ENTERTAINMENT_COMICS,
+    21: Category.SCIENCE_GADGETS,
+    22: Category.ENTERTAINMENT_JAPANESE_ANIME_MANGA,
+    23: Category.ENTERTAINMENT_CARTOON_ANIMATIONS,
+}
+
 
 class AskTriviaQuestionsCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -15,45 +42,78 @@ class AskTriviaQuestionsCog(commands.Cog):
         name="ask-trivia-questions", description="Asks trivia questions!"
     )
     @app_commands.describe(difficulty="choose a difficulty")
+    @app_commands.describe(category="choose a category")
     @app_commands.choices(
         difficulty=[
             app_commands.Choice(name="Easy", value=1),
             app_commands.Choice(name="Medium", value=2),
             app_commands.Choice(name="Hard", value=3),
-        ]
+        ],
+        category=[
+            app_commands.Choice(name="General Knowledge", value=1),
+            app_commands.Choice(name="Entertainment: Books", value=2),
+            app_commands.Choice(name="Entertainment: Film", value=3),
+            app_commands.Choice(name="Entertainment: Music", value=4),
+            app_commands.Choice(name="Entertainment: Musicals & Theatres", value=5),
+            app_commands.Choice(name="Entertainment: Television", value=6),
+            app_commands.Choice(name="Entertainment: Video Games", value=7),
+            app_commands.Choice(name="Entertainment: Board Games", value=8),
+            app_commands.Choice(name="Science & Nature", value=9),
+            app_commands.Choice(name="Science: Computers", value=10),
+            app_commands.Choice(name="Mythology", value=11),
+            app_commands.Choice(name="Sports", value=12),
+            app_commands.Choice(name="Geography", value=13),
+            app_commands.Choice(name="History", value=14),
+            app_commands.Choice(name="Politics", value=15),
+            app_commands.Choice(name="Art", value=16),
+            app_commands.Choice(name="Celebrities", value=17),
+            app_commands.Choice(name="Animals", value=18),
+            app_commands.Choice(name="Vehicles", value=19),
+            app_commands.Choice(name="Entertainment: Comics", value=20),
+            app_commands.Choice(name="Science: Gadgets", value=21),
+            app_commands.Choice(name="Entertainment: Japanese Anime & Manga", value=22),
+            app_commands.Choice(name="Entertainment: Cartoon & Animations", value=23),
+        ],
     )
     async def ask_trivia_question(
-        self, inter: discord.Interaction, difficulty: app_commands.Choice[int]
+        self,
+        inter: discord.Interaction,
+        difficulty: app_commands.Choice[int],
+        category: app_commands.Choice[int],
     ):
         await inter.response.defer()
 
         # Reset the answered_users set
         self.answered_users = set()
 
-        # Create a client to retrieve 1 "General Knowledge" question with the specified difficulty
+        # Convert the selected category value to value
+        selected_category = category_mapping.get(category.value, None)
+
+        # Create a client to retrieve 1 question with the specified difficulty and category
         client = OpenTDBClient()
 
         if difficulty.value == 1:
             question_set = client.get_questions(
                 amount=1,
-                category=Category.GENERAL_KNOWLEDGE,
+                category=selected_category,
                 difficulty=Difficulty.EASY,
             )
         elif difficulty.value == 2:
             question_set = client.get_questions(
                 amount=1,
-                category=Category.GENERAL_KNOWLEDGE,
+                category=selected_category,
                 difficulty=Difficulty.MEDIUM,
             )
         elif difficulty.value == 3:
             question_set = client.get_questions(
                 amount=1,
-                category=Category.GENERAL_KNOWLEDGE,
+                category=selected_category,
                 difficulty=Difficulty.HARD,
             )
 
         question_txt = question_set.items[0].question
         question_diff = question_set.items[0].difficulty.name.title()
+        #  question_cat = selected_category.name.title()
         choices = question_set.items[0].choices
         answer_indx = question_set.items[0].answer_index
 
@@ -63,7 +123,7 @@ class AskTriviaQuestionsCog(commands.Cog):
             color=discord.Color.orange(),
         )
 
-        # changed from self to self.inccorect
+        # changed from self to self.incorrect
         view = MyView(
             choices,
             answer_indx,
