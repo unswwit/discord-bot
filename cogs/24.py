86a89148eb5a589642436f50793080ce2a4085a2
Joyce <joyce.cy.he@gmail.com>
2023-09-26 23:52:58 +1000

    Adding stub code for 24 command

--- cogs/24.py
+++ cogs/24.py
@@ -1,17 +1,27 @@
-import discord 
+import discord
 import math
-import random 
+import random
 from discord.ext import commands
 from discord import app_commands
 
-class TwentyFourCog(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
+# 24 game
+# /24 to play game
+# Give us 4 random numbers that can make 24
+# Numpad (4x3) of only those 4 numbers, + arithmetic, brackets, equals
+# Clicking equals runs solver command to see if statement makes 24
+# Correct window
+# Incorrect window
 
-    @app_commands.command(name="24", description="Play a game of 24!")
-    async def play24(
-        self,
-        inter: discord.Interaction,
-    ):
-        await inter.response.defer()
+# class TwentyFourCog(commands.Cog):
+# Setting up 24 game
 
+# Numpad
+# class NumberInputInterface(discord.ui.View):
+# async def callback(self, interaction: discord.Interaction):
+
+# async def updateMessageIncorrect(self, interaction: discord.Interaction):
+
+# async def updateMessageCorrect(self, interaction: discord.Interaction):
+
+# async def setup(bot: commands.Bot):
+# await bot.add_cog(TwentyFourCog(bot))
