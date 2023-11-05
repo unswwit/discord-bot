2474a62bba34569f8ffc9e5250cfb071803e2af7
katejoh <kate.oh13@gmail.com>
2023-11-05 23:52:22 +1100

    feat: add buttons to 24 command view

--- cogs/24.py
+++ cogs/24.py
@@ -154,7 +154,7 @@ class TwentyFourCog(commands.Cog):
             return
 
         embed = discord.Embed(
-            title="24", 
+            title="24",
             color=discord.Color.orange()
         )
 
@@ -164,16 +164,88 @@ class TwentyFourCog(commands.Cog):
             inline=False,
         )
 
-        view = MyView(id)
+        view = MyView(id, random_combination)
         await interaction.followup.send(
             embed=embed,
             view=view,
+            ephemeral=True
         )
 
 class MyView(discord.ui.View):
-    def __init__(self, creator_id):
+    def __init__(self, creator_id, numbers):
         super().__init__(timeout=None)
         self.creator_id = creator_id
+        self.numbers = [str(number) for number in numbers]
+        self.buttons = []
+        self.create_buttons()
+        self.current_input = ""
+
+    def create_buttons(self):
+        # number buttons
+        for number in self.numbers:
+            button_callback = self.create_button_callback(number)
+            button = discord.ui.Button(
+                style=discord.ButtonStyle.green,
+                label=number,
+                custom_id=number,
+                row=0,
+            )
+            button.callback = button_callback
+            self.buttons.append(button)
+
+        # calculator buttons
+        calculator_operations = ["+", "-", "×", "÷", "(", ")", "="]
+        cur_row = 1
+        colour = discord.ButtonStyle.grey
+        for operation in calculator_operations:
+            # parentheses and equals on a new row
+            if operation == "(":
+                cur_row += 1
+            elif operation == "=":
+                colour = discord.ButtonStyle.blurple
+
+            button_callback = self.create_button_callback(operation)
+            button = discord.ui.Button(
+                style=colour,
+                label=operation, custom_id=operation,
+                row=cur_row,
+            )
+            button.callback = button_callback
+            self.buttons.append(button)
+
+        for button in self.buttons:
+            self.add_item(button)
+
+    def create_button_callback(self, button_id):
+        # Runs when a button is pressed
+        async def button_callback(interaction):
+            self.current_input += " " + button_id
+            for item in self.children:
+                if item.custom_id == button_id:
+                    # disable button if a number
+                    if button_id in self.numbers:
+                        item.disabled = True
+
+            await self.update_message(interaction)
+
+        return button_callback
+
+    async def update_message(self, interaction):
+        embed = discord.Embed(
+            title="24",
+            color=discord.Color.orange()
+        )
+
+        embed.add_field(
+            name="Your numbers are:",
+            value=f"` {self.numbers[0]}   {self.numbers[1]}   {self.numbers[2]}   {self.numbers[3]} `",
+            inline=False,
+        )
+
+        embed.add_field(
+            name="Input:", value="`" + self.current_input + "`", inline=False
+        )
+        await interaction.response.edit_message(embed=embed, view=self)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(TwentyFourCog(bot))
