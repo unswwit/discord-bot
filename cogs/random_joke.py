6e975a01f3cfbacdae25510df3a3081d3f9c5290
Aishwarya Sundar <aishwaryasundar31@gmail.com>
2024-07-30 16:41:46 +1000

    feat/WIT-216: random joke command

--- cogs/random_joke.py
+++ cogs/random_joke.py
@@ -5,21 +5,23 @@ import requests
 
 link = "https://official-joke-api.appspot.com/jokes/random"
 
+
 class RandomJokeCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(
-        name="random-joke", description="Tell a random joke!"
+            name="random-joke", description="Tell a random joke!"
     )
+    
     async def send_random_joke(self, inter: discord.Interaction):
         response = requests.get(link)
         if response.status_code == 200:
-            data = response.json() 
+            data = response.json()
             message = f"**Topic:** {data.get('type').capitalize()}\n**The Joke:** {data.get('setup')}\n**Punchline:** {data.get('punchline')}"
             await inter.response.send_message(message)
         else:
-             await inter.response.send_message("Failed to retrieve random joke")
+            await inter.response.send_message("Failed to retrieve random joke")
 
 
 async def setup(bot: commands.Bot):
