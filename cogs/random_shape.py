89c47e1c198b43e204bd80716e8f98cfb29b50e5
bmitanshi <barmitanshi@gmail.com>
2025-02-02 16:43:19 +1100

    integrated generate_random_shape function

--- cogs/random_shape.py
+++ cogs/random_shape.py
@@ -1,7 +1,8 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from shapes import shapes_list
+# from shapes import shapes_list
+from gen_shape import generate_random_shape
 import random
 
 class RandomShapeGenerate(commands.Cog):
@@ -11,8 +12,19 @@ class RandomShapeGenerate(commands.Cog):
         name="random-shape", description="Give a random shape!"
     )
     async def send_random_object(self, inter: discord.Interaction):
-        random_shape = random.choice(shapes_list)
-        await inter.response.send_message(random_shape["name"])
+        # random_shape = random.choice(shapes_list)
+        # await inter.response.send_message(random_shape["name"])
+        
+        # Generate the random shape and save it as a file
+        generate_random_shape()
+
+        # Open the saved image to send it to Discord
+        with open("random_shape.png", "rb") as f:
+            image = discord.File(f, filename="random_shape.png")
+
+        # Send the image as a message
+        await inter.response.send_message("Here's a random shape for you!", file=image)
+
         
 async def setup(bot: commands.Bot):
     await bot.add_cog(RandomShapeGenerate(bot))
